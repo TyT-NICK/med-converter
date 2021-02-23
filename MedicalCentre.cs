44b93459eb1e500bb86c714d817f3fa1d97c6b47
TyT-NICK <artem99k14@gmail.com>
2021-02-23 22:38:07 +0800

    Добавьте файлы проекта.

---
+++ MedicalCentre.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+
+namespace medConvert
+{
+    class MedicalCentre
+    {
+        private static List<MedicalCentre> centres = new List<MedicalCentre>();
+        public static List<MedicalCentre> Centres { get; private set; }
+
+        private List<Rate> rates = new List<Rate>();
+
+        public int          Id { get; private set; }
+        public string       FullName { get; private set; }
+        public string       City { get; private set; }
+        public List<Rate>   Rates { get; private set; }
+
+        public static void InsertAllToDB()
+        {
+            const string server = "127.0.0.1",
+                         userId = "root",
+                         password = "root",
+                         database = "medicine",
+                         port = "3306";
+
+            MySqlConnection sqlClient = new MySqlConnection($"Server={server};UserId={userId};Password={password};Database={database}");
+            Debug.WriteLineIf(sqlClient.Ping(), "Соединение с БД установлено");
+
+            sqlClient.Open();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM med_uchr", sqlClient);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            sqlClient.Close();
+        }
+
+        public MedicalCentre(string fullName, string city)
+        {
+            this.FullName   = fullName;
+            this.City       = city;
+
+            Debug.WriteLine(this);
+            centres.Add(this);
+            Debug.WriteLine(centres.Count);
+        }
+
+        override public string ToString()
+        {
+            return $"{this.FullName} - {this.City}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+using MySqlX;
+
+namespace medConvert
+{
+    class Program
+    {
+        static void ReadFile()
+        {
+            string filePath = $@"{Environment.CurrentDirectory}\med.xlsx";
+            FileInfo file = new FileInfo(filePath);
+
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet sheet = package.Workbook.Worksheets[1];
+
+                for (int i = 5; i <= 29; i++)
+                {
+                    // [Row, Column]
+                    string fullName = sheet.Cells[i, 2].Value.ToString();
+                    string city = sheet.Cells[i, 5].Value.ToString();
+
+                    new MedicalCentre(fullName, city);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //ReadFile();
+            MedicalCentre.InsertAllToDB();
+        }
+    }
+}
